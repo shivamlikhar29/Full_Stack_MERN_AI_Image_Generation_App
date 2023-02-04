246133166dafd4e3bc6682a9265f4783fbb65cb5
Shivam <shivam1likhar14209211@gmail.com>
2023-02-04 15:18:49 +0530

    url done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8000/api/v1/post', {
+      const response = await fetch('https://ai-image-q3pw.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -35,7 +35,7 @@ const Home = () => {
 
       if (response.ok) {
         const result = await response.json();
-        // console.log(result.data)
+        console.log(result.data)
         setAllPosts(result.data.reverse());
       }
     } catch (err) {
